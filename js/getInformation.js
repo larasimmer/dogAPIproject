358fafd3467f63d99738dd9b5ea6fe7560ea9bcf
WBJ-000159 <larafsimmer@gmail.com>
2023-03-08 14:59:14 -0300

    Styling the project

--- js/getInformation.js
+++ js/getInformation.js
@@ -1,4 +1,6 @@
 let divInformationContainer = document.getElementById("information-container");
+let buttonRandom = document.getElementById('get-information');
+let image = document.getElementById('image');
 
 async function getInformation() {
     let response = await fetch('https://api.thedogapi.com/v1/images/search');
@@ -9,11 +11,13 @@ async function getInformation() {
 
 async function showInformation() {
     let information = await getInformation();
-    let image = document.createElement("img");
 
-    image.setAttribute("src", information['url']);
+    image.setAttribute("src", information.url);
     divInformationContainer.appendChild(image);
 }
 
-showInformation();
+buttonRandom.addEventListener('click',function() {
+    divInformationContainer.innerHTML = '';
+    showInformation();
+});
 
