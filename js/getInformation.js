c3b786a243f68f26a80900875719da1f9d73322e
WBJ-000159 <larafsimmer@gmail.com>
2023-03-10 11:26:41 -0300

    Adding breed and temperament information

--- js/getInformation.js
+++ js/getInformation.js
@@ -1,53 +1,34 @@
 let divInformationContainer = document.getElementById('information-container');
 let buttonRandom = document.getElementById('get-information');
 let image = document.getElementById('image');
+let breedSpan = document.getElementById('breed');
+let temperamentSpan = document.getElementById('temperament');
 
-async function getImage() {
-    let response = await fetch('https://api.thedogapi.com/v1/images/search');
+async function getInformation() {
+    let response = await fetch('https://api.thedogapi.com/v1/breeds?limit=10&page=0');
     let data = await response.json();
+    let randomNumber = Math.floor(Math.random() * 10);
 
-    return data[0];
+    return data[randomNumber];
 }
-
-function getBreed() {
-    $.ajax({
-        url: "https://api.thedogapi.com/v1/breeds?limit=10&page=0",
-        method: "GET",
-        timeout: 0,
-        headers: {
-          "Content-Type": "application/json",
-          "x-api-key": "live_jO3TYHsD3aur2dghaat9o9GXXbu3Hr2oj5G6kQ0GBt0KkF0nYTYFtUsg461HerxC"
-        },
-        beforeSend: function() {
-            console.log("Enviando...");
-        }
-    })
-    .done(function(response) {
-        let randomNumber = Math.floor(Math.random() * 10);
-        console.log(response[randomNumber]);
-    })
-    .fail(function(err) {
-        let errorMessage = "Um problema ocorreu na geração da imagem. Erro: " + err.status + ".";
-        divInformationContainer.innerHTML = "<p>" + errorMessage + "</p";
-    })
-};
 
 async function showInformation() {
     let information;
 
     try {
-        information = await getImage();
+        information = await getInformation();
     } catch (err) {
-        let errorMessage = "Um problema ocorreu na geração da imagem. Erro: " + err.status + ".";
+        let errorMessage = "Um problema ocorreu na geração das informações. Erro: " + err.status + ".";
         divInformationContainer.innerHTML = "<p>" + errorMessage + "</p";
     };
 
-    getBreed();
-    image.setAttribute("src", information.url);
+    image.setAttribute("src", information.image.url);
     divInformationContainer.appendChild(image);
+    breedSpan.innerText = information.name;
+    temperamentSpan.innerText = information.temperament;
 }
 
 buttonRandom.addEventListener('click',function() {
-    divInformationContainer.innerHTML = '';
+    image.innerHTML = '';
     showInformation();
 });
